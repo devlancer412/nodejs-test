b0bd6eee271dc13bf94a7fbac41a14208bba7a04
Joseph Anderson <devanderson0412@gmail.com>
2022-07-23 11:20:36 -0500

    test case divided

--- frontend/src/App.cy.tsx
+++ frontend/src/App.cy.tsx
@@ -2,14 +2,25 @@ import { mount } from '@cypress/react';
 import App from './App';
 
 describe('<App>', () => {
-  it('mounts', () => {
+  before('mounts', () => {
     mount(<App />)
+  })
 
+  it('5 + 3 = 8', () => {
     cy.get('input#first').type('5');
     cy.get('input#second').type('3');
 
     cy.get('button').click();
 
     cy.get('input#sum').should('have.value', 8);
+  })
+
+  it('5 + -3 = 2', () => {
+    cy.get('input#first').type('5');
+    cy.get('input#second').type('-3');
+
+    cy.get('button').click();
+
+    cy.get('input#sum').should('have.value', 2);
   })
 })
